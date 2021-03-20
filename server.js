664dd678f2c15f560e9744a8adb9a94dc10b750a
Jonathan Pang <pangj@bxscience.edu>
2021-03-20 09:41:21 -0400

    addd post and specific get request

--- server.js
+++ server.js
@@ -19,5 +19,43 @@ app.get('/users/Jonathan338833&&', (req, res) => {
     })
 })
 
+// /GET specific user 
+app.get('/users/:email/:password', (req, res) => {
+    mongodb.connect(mongodb_URI, function(error, db) {
+        if (error) throw error
+        var dbo = db.db('FundraiserApp')
+        var query = { email: req.params.email, password: req.params.password }
+        dbo.collection('Users').find(query).toArray(function(error, result) { 
+            if (error) throw error
+            res.send(result)
+            console.log(result)
+            db.close()
+        })
+    })
+})
+
+// /POST new user
+app.post('/users/:id/:firstName/:lastName/:email/:password/:school', (req, res) => {
+    const user = {
+        id: req.body.id,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+        school: req.body.school
+    }
+
+    mongodb.connect(mongodb_URI, function (error, db) {
+        if (error) throw error;
+        var dbo = db.db('FundraiserApp')
+        dbo.collection('Users').insertOne(user, function(error, result) {
+            if (error) throw error;
+            assert.equal(null, error)
+            console.log('Item inserted')
+            db.close();
+        })
+    })
+})
+
 const port = process.env.PORT || 2000
 app.listen(port, () => console.log('Listening on ' + port + '...'))
